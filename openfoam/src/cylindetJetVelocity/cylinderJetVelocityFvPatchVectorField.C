654142ba0d6cff9185c172d5a958c278de43cc5e
darshan315 <darshankbr@gmail.com>
2023-07-01 01:16:13 +0200

    added src files

--- openfoam/src/cylindetJetVelocity/cylinderJetVelocityFvPatchVectorField.C
+++ openfoam/src/cylindetJetVelocity/cylinderJetVelocityFvPatchVectorField.C
@@ -24,7 +24,7 @@ License
     along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
 
 \*---------------------------------------------------------------------------*/
-#include "pinballRotatingWallVelocityFvPatchVectorField.H"
+#include "cylinderJetVelocityFvPatchVectorField.H"
 #include "addToRunTimeSelectionTable.H"
 #include "volFields.H"
 #include "surfaceFields.H"
@@ -32,14 +32,12 @@ License
 
 // * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
 
-Foam::pinballRotatingWallVelocityFvPatchVectorField::
-    pinballRotatingWallVelocityFvPatchVectorField(
+Foam::cylinderJetVelocityFvPatchVectorField::
+    cylinderJetVelocityFvPatchVectorField(
         const fvPatch &p,
         const DimensionedField<vector, volMesh> &iF)
     : fixedValueFvPatchField<vector>(p, iF),
-      origin_a_(),
-      origin_b_(),
-      origin_c_(),
+      origin(),
       axis_(Zero),
       probes_(initializeProbes()),
       control_(initializeControl())
@@ -47,31 +45,27 @@ Foam::pinballRotatingWallVelocityFvPatchVectorField::
     initializeCylinderSegmentation();
 }
 
-Foam::pinballRotatingWallVelocityFvPatchVectorField::
-    pinballRotatingWallVelocityFvPatchVectorField(
+Foam::cylinderJetVelocityFvPatchVectorField::
+    cylinderJetVelocityFvPatchVectorField(
         const fvPatch &p,
         const DimensionedField<vector, volMesh> &iF,
         const dictionary &dict)
     : fixedValueFvPatchField<vector>(p, iF, dict, false),
-      origin_a_(dict.get<vector>("origin_a")),
-      origin_b_(dict.get<vector>("origin_b")),
-      origin_c_(dict.get<vector>("origin_c")),
+      origin_(dict.get<vector>("origin_")),
       axis_(dict.get<vector>("axis")),
       train_(dict.get<bool>("train")),
       policy_name_(dict.get<word>("policy")),
       policy_(torch::jit::load(policy_name_)),
-      abs_omega_max_(dict.get<scalar>("absOmegaMax")),
+      abs_Q_max_(dict.get<scalar>("absQMax")),
       seed_(dict.get<int>("seed")),
       probes_name_(dict.get<word>("probesDict")),
       gen_(seed_),
-      omega_a_(0.0),
-      omega_old_a_(0),
-      omega_b_(0.0),
-      omega_old_b_(0),
-      omega_c_(0.0),
-      omega_old_c_(0),
+      Q_upper_(0.0),
+      Q_old_upper_(0),
+      Q_lower_(0.0),
+      Q_old_lower_(0),
       control_time_(0.0),
-      update_omega_(true),
+      update_Q_(true),
       probes_(initializeProbes()),
       control_(initializeControl())
 {
@@ -80,91 +74,79 @@ Foam::pinballRotatingWallVelocityFvPatchVectorField::
 }
 
 
-Foam::pinballRotatingWallVelocityFvPatchVectorField::
-    pinballRotatingWallVelocityFvPatchVectorField(
-        const pinballRotatingWallVelocityFvPatchVectorField &ptf,
+Foam::cylinderJetVelocityFvPatchVectorField::
+    cylinderJetVelocityFvPatchVectorField(
+        const cylinderJetVelocityFvPatchVectorField &ptf,
         const fvPatch &p,
         const DimensionedField<vector, volMesh> &iF,
         const fvPatchFieldMapper &mapper)
     : fixedValueFvPatchField<vector>(ptf, p, iF, mapper),
-      origin_a_(ptf.origin_a_),
-      origin_b_(ptf.origin_b_),
-      origin_c_(ptf.origin_c_),
+      origin_(ptf.origin_),
       axis_(ptf.axis_),
       train_(ptf.train_),
       policy_name_(ptf.policy_name_),
       policy_(ptf.policy_),
-      abs_omega_max_(ptf.abs_omega_max_),
+      abs_Q_max_(ptf.abs_Q_max_),
       seed_(ptf.seed_),
       probes_name_(ptf.probes_name_),
       gen_(ptf.gen_),
-      omega_a_(ptf.omega_a_),
-      omega_old_a_(ptf.omega_old_a_),
-      omega_b_(ptf.omega_b_),
-      omega_old_b_(ptf.omega_old_b_),
-      omega_c_(ptf.omega_c_),
-      omega_old_c_(ptf.omega_old_c_),
+      Q_upper_(ptf.Q_upper_),
+      Q_old_upper_(ptf.Q_old_upper_),
+      Q_lower_(ptf.Q_lower_),
+      Q_old_lower_(ptf.Q_old_lower_),
       control_time_(ptf.control_time_),
-      update_omega_(ptf.update_omega_),
+      update_Q_(ptf.update_Q_),
       probes_(initializeProbes()),
       control_(initializeControl())
 {
     initializeCylinderSegmentation();
 }
 
-Foam::pinballRotatingWallVelocityFvPatchVectorField::
-    pinballRotatingWallVelocityFvPatchVectorField(
-        const pinballRotatingWallVelocityFvPatchVectorField &rwvpvf)
+Foam::cylinderJetVelocityFvPatchVectorField::
+    cylinderJetVelocityFvPatchVectorField(
+        const cylinderJetVelocityFvPatchVectorField &rwvpvf)
     : fixedValueFvPatchField<vector>(rwvpvf),
-      origin_a_(rwvpvf.origin_a_),
-      origin_b_(rwvpvf.origin_b_),
-      origin_c_(rwvpvf.origin_c_),
+      origin_(rwvpvf.origin_),
       axis_(rwvpvf.axis_),
       train_(rwvpvf.train_),
       policy_name_(rwvpvf.policy_name_),
       policy_(rwvpvf.policy_),
-      abs_omega_max_(rwvpvf.abs_omega_max_),
+      abs_Q_max_(rwvpvf.abs_Q_max_),
       seed_(rwvpvf.seed_),
       probes_name_(rwvpvf.probes_name_),
       gen_(rwvpvf.gen_),
-      omega_a_(rwvpvf.omega_a_),
-      omega_old_a_(rwvpvf.omega_old_a_),
-      omega_b_(rwvpvf.omega_b_),
-      omega_old_b_(rwvpvf.omega_old_b_),
-      omega_c_(rwvpvf.omega_c_),
-      omega_old_c_(rwvpvf.omega_old_c_),
+      Q_upper_(rwvpvf.Q_upper_),
+      Q_old_upper_(rwvpvf.Q_old_upper_),
+      Q_lower_(rwvpvf.Q_lower_),
+      Q_old_lower_(rwvpvf.Q_old_lower_),
       control_time_(rwvpvf.control_time_),
-      update_omega_(rwvpvf.update_omega_),
+      update_Q_(rwvpvf.update_Q_),
       probes_(initializeProbes()),
       control_(initializeControl())
 {
     initializeCylinderSegmentation();
 }
 
-Foam::pinballRotatingWallVelocityFvPatchVectorField::
-    pinballRotatingWallVelocityFvPatchVectorField(
-        const pinballRotatingWallVelocityFvPatchVectorField &rwvpvf,
+Foam::cylinderJetVelocityFvPatchVectorField::
+    cylinderJetVelocityFvPatchVectorField(
+        const cylinderJetVelocityFvPatchVectorField &rwvpvf,
         const DimensionedField<vector, volMesh> &iF)
     : fixedValueFvPatchField<vector>(rwvpvf, iF),
-      origin_a_(rwvpvf.origin_a_),
-      origin_b_(rwvpvf.origin_b_),
-      origin_c_(rwvpvf.origin_c_),
+      origin_(rwvpvf.origin_),
       axis_(rwvpvf.axis_),
       train_(rwvpvf.train_),
       policy_name_(rwvpvf.policy_name_),
       policy_(rwvpvf.policy_),
-      abs_omega_max_(rwvpvf.abs_omega_max_),
+      abs_Q_max_(rwvpvf.abs_Q_max_),
       seed_(rwvpvf.seed_),
       probes_name_(rwvpvf.probes_name_),
       gen_(rwvpvf.gen_),
-      omega_a_(rwvpvf.omega_a_),
-      omega_old_a_(rwvpvf.omega_old_a_),
-      omega_b_(rwvpvf.omega_b_),
-      omega_old_b_(rwvpvf.omega_old_b_),
-      omega_c_(rwvpvf.omega_c_),
-      omega_old_c_(rwvpvf.omega_old_c_),
+      Q_upper_(rwvpvf.Q_upper_),
+      Q_old_upper_(rwvpvf.Q_old_upper_),
+      Q_lower_(rwvpvf.Q_lower_),
+      Q_old_lower_(rwvpvf.Q_old_lower_),
       control_time_(rwvpvf.control_time_),
-      update_omega_(rwvpvf.update_omega_),
+      update_Q_(rwvpvf.update_Q_),
       probes_(initializeProbes()),
       control_(initializeControl())
 {
@@ -173,7 +155,7 @@ Foam::pinballRotatingWallVelocityFvPatchVectorField::
 
 // * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
 
-void Foam::pinballRotatingWallVelocityFvPatchVectorField::updateCoeffs()
+void Foam::cylinderJetVelocityFvPatchVectorField::updateCoeffs()
 {
     if (updated())
     {
@@ -189,11 +171,10 @@ void Foam::pinballRotatingWallVelocityFvPatchVectorField::updateCoeffs()
                          t >= start_time_ - 0.5*dt &&
                          timeIndex != startTimeIndex;
 
-    if (timeToControl && update_omega_)
+    if (timeToControl && update_Q_)
     {
-        omega_old_a_ = omega_a_;
-        omega_old_b_ = omega_b_;
-        omega_old_c_ = omega_c_;
+        Q_old_upper_ = Q_upper_;
+        Q_old_lower_ = Q_lower_;
         control_time_ = t;
 
         const volScalarField& p = this->db().lookupObject<volScalarField>("p"); 
@@ -206,112 +187,89 @@ void Foam::pinballRotatingWallVelocityFvPatchVectorField::updateCoeffs()
             );
             std::vector<torch::jit::IValue> policyFeatures{features};
             torch::Tensor dist_parameters = policy_.forward(policyFeatures).toTensor();
-            scalar alpha_a = dist_parameters[0][0].item<double>();
-            scalar alpha_b = dist_parameters[0][1].item<double>();
-            scalar alpha_c = dist_parameters[0][2].item<double>();
-            scalar beta_a = dist_parameters[0][3].item<double>();
-            scalar beta_b = dist_parameters[0][4].item<double>();
-            scalar beta_c = dist_parameters[0][5].item<double>();
-            std::gamma_distribution<double> distribution_1_a(alpha_a, 1.0);
-            std::gamma_distribution<double> distribution_2_a(alpha_b, 1.0);
-            std::gamma_distribution<double> distribution_3_a(alpha_c, 1.0);
-            std::gamma_distribution<double> distribution_1_b(beta_a, 1.0);
-            std::gamma_distribution<double> distribution_2_b(beta_b, 1.0);
-            std::gamma_distribution<double> distribution_3_b(beta_c, 1.0);
-            scalar omega_pre_scale_a;
-            scalar omega_pre_scale_b;
-            scalar omega_pre_scale_c;
+            scalar alpha_upper = dist_parameters[0][0].item<double>();
+            scalar alpha_lower = dist_parameters[0][1].item<double>();
+            scalar beta_upper = dist_parameters[0][3].item<double>();
+            scalar beta_lower = dist_parameters[0][4].item<double>();
+            std::gamma_distribution<double> distribution_1_a(alpha_upper, 1.0);
+            std::gamma_distribution<double> distribution_2_a(alpha_lower, 1.0);
+            std::gamma_distribution<double> distribution_1_b(beta_upper, 1.0);
+            std::gamma_distribution<double> distribution_2_b(beta_lower, 1.0);
+            scalar Q_pre_scale_upper;
+            scalar Q_pre_scale_lower;
             if (train_)
             {
                 // sample from Beta distribution during training
                 double number_1_a = distribution_1_a(gen_);
                 double number_2_a = distribution_2_a(gen_);
-                double number_3_a = distribution_3_a(gen_);
                 double number_1_b = distribution_1_b(gen_);
                 double number_2_b = distribution_2_b(gen_);
-                double number_3_b = distribution_3_b(gen_);
-                omega_pre_scale_a = number_1_a / (number_1_a + number_1_b);
-                omega_pre_scale_b = number_2_a / (number_2_a + number_2_b);
-                omega_pre_scale_c = number_3_a / (number_3_a + number_3_b);
+                Q_pre_scale_upper = number_1_a / (number_1_a + number_1_b);
+                Q_pre_scale_lower = number_2_a / (number_2_a + number_2_b);
             }
             else
             {
                 // use expected (mean) angular velocity
-                omega_pre_scale_a = alpha_a / (alpha_a + beta_a);
-                omega_pre_scale_b = alpha_b / (alpha_b + beta_b);
-                omega_pre_scale_c = alpha_c / (alpha_c + beta_c);
+                Q_pre_scale_upper = alpha_upper / (alpha_upper + beta_upper);
+                Q_pre_scale_lower = alpha_lower / (alpha_lower + beta_lower);
             }
             // rescale to actionspace
-            omega_a_ = (omega_pre_scale_a - 0.5) * 2 * abs_omega_max_;
-            omega_b_ = (omega_pre_scale_b - 0.5) * 2 * abs_omega_max_;
-            omega_c_ = (omega_pre_scale_c - 0.5) * 2 * abs_omega_max_;
+            Q_upper_ = (Q_pre_scale_upper - 0.5) * 2 * abs_Q_max_;
+            Q_lower_ = (Q_pre_scale_lower - 0.5) * 2 * abs_Q_max_;
             // save trajectory
-            saveTrajectory(alpha_a, beta_a, alpha_b, beta_b, alpha_c, beta_c);
-            Info << "New omega_a: " << omega_a_ << "; old value: " << omega_old_a_ << "\n";
-            Info << "New omega_b: " << omega_b_ << "; old value: " << omega_old_b_ << "\n";
-            Info << "New omega_c: " << omega_c_ << "; old value: " << omega_old_c_ << "\n";
+            saveTrajectory(alpha_upper, beta_upper, alpha_lower, beta_lower);
+            Info << "New Q_upper: " << Q_upper_ << "; old value: " << Q_old_upper_ << "\n";
+            Info << "New Q_lower: " << Q_lower_ << "; old value: " << Q_old_lower_ << "\n";
         }
-        Pstream::scatter(omega_a_);
-        Pstream::scatter(omega_b_);
-        Pstream::scatter(omega_c_);
+        Pstream::scatter(Q_upper_);
+        Pstream::scatter(Q_lower_);
         // avoid update of angular velocity during p-U coupling
-        update_omega_ = false;
+        update_Q_ = false;
     }
 
     // activate update of angular velocity after p-U coupling
-    if (!timeToControl && !update_omega_)
+    if (!timeToControl && !update_Q_)
     {
-        update_omega_ = true;
+        update_Q_ = true;
     }
 
     // update angular velocity by linear transition from old to new value
-    scalar d_omega_a = (omega_a_ - omega_old_a_) / dt_control_ * (t - control_time_);
-    scalar omega_a = omega_old_a_ + d_omega_a;
-    scalar d_omega_b = (omega_b_ - omega_old_b_) / dt_control_ * (t - control_time_);
-    scalar omega_b = omega_old_b_ + d_omega_b;
-    scalar d_omega_c = (omega_c_ - omega_old_c_) / dt_control_ * (t - control_time_);
-    scalar omega_c = omega_old_c_ + d_omega_c;
-
+    scalar d_Q_upper = (Q_upper_ - Q_old_upper_) / dt_control_ * (t - control_time_);
+    scalar Q_upper = Q_old_upper_ + d_Q_upper;
+    scalar d_Q_lower = (Q_lower_ - Q_old_lower_) / dt_control_ * (t - control_time_);
+    scalar Q_lower = Q_old_lower_ + d_Q_lower;
     const int patch_size = (patch().Cf()).size();
     vectorField Up(patch_size);
-    forAll(faces_a_, faceI)
+    forAll(faces_upper_, faceI)
     {
-        label faceAI = faces_a_[faceI];
-        Up[faceAI] = (-omega_a) * ((centers_a_[faceI] - origin_a_) ^ (axis_ / mag(axis_)));
-        Up[faceAI] -= normals_a_[faceI] * (normals_a_[faceI] & Up[faceAI]);
+        label faceuI = faces_a_[faceI];
+        Up[faceuUI] = (-Q_upper) * ((centers_upper_[faceI] - origin_) ^ (axis_ / mag(axis_)));
+        Up[faceUI] -= normals_upper_[faceI] * (normals_upper_[faceI] & Up[faceUI]);
     }
-    forAll(faces_b_, faceI)
+    forAll(faces_lower_, faceI)
     {
-        label faceBI = faces_b_[faceI];
-        Up[faceBI] = (-omega_b) * ((centers_b_[faceI] - origin_b_) ^ (axis_ / mag(axis_)));
-        Up[faceBI] -= normals_b_[faceI] * (normals_b_[faceI] & Up[faceBI]);
-    }
-    forAll(faces_c_, faceI)
-    {
-        label faceCI = faces_c_[faceI];
-        Up[faceCI] = (-omega_c) * ((centers_c_[faceI] - origin_c_) ^ (axis_ / mag(axis_)));
-        Up[faceCI] -= normals_c_[faceI] * (normals_c_[faceI] & Up[faceCI]);
+        label faceLI = faces_lower_[faceI];
+        Up[faceLI] = (-Q_lower) * ((centers_lower_[faceI] - origin_) ^ (axis_ / mag(axis_)));
+        Up[faceLI] -= normals_lower_[faceI] * (normals_lower_[faceI] & Up[faceLI]);
     }
 
     vectorField::operator=(Up);
     fixedValueFvPatchVectorField::updateCoeffs();
 }
 
-void Foam::pinballRotatingWallVelocityFvPatchVectorField::write(Ostream &os) const
+void Foam::cylinderJetVelocityFvPatchVectorField::write(Ostream &os) const
 {
     fvPatchVectorField::write(os);
-    os.writeEntry("origin_a", origin_a_);
-    os.writeEntry("origin_b", origin_b_);
-    os.writeEntry("origin_c", origin_c_);
+    os.writeEntry("origin_", origin_);
     os.writeEntry("axis", axis_);
     os.writeEntry("policy", policy_name_);
     os.writeEntry("train", train_);
-    os.writeEntry("absOmegaMax", abs_omega_max_);
+    os.writeEntry("absQMax", abs_Q_max_);
     os.writeEntry("seed", seed_);
     os.writeEntry("probesDict", probes_name_);
 }
 
-void Foam::pinballRotatingWallVelocityFvPatchVectorField::saveTrajectory(scalar alpha_a, scalar beta_a, scalar alpha_b, scalar beta_b, scalar alpha_c, scalar beta_c) const
+void Foam::cylinderJetVelocityFvPatchVectorField::saveTrajectory(scalar alpha_upper, scalar beta_upper, scalar alpha_lower, scalar beta_lower) const
 {
     std::ifstream file("trajectory.csv");
     std::fstream trajectory("trajectory.csv", std::ios::app | std::ios::binary);
@@ -320,26 +278,23 @@ void Foam::pinballRotatingWallVelocityFvPatchVectorField::saveTrajectory(scalar 
     if(!file.good())
     {
         // write header
-        trajectory << "t, omega_a, alpha_a, beta_a, omega_b, alpha_b, beta_b, omega_c, alpha_c, beta_c";
+        trajectory << "t, Q_upper, alpha_upper, beta_upper, Q_lower, alpha_lower, beta_lower";
     }
 
     trajectory << std::setprecision(15)
                << "\n"
                << t << ", "
-               << omega_a_ << ", "
-               << alpha_a << ", "
-               << beta_a << ", "
-               << omega_b_ << ", "
-               << alpha_b << ", "
-               << beta_b << ", "
-               << omega_c_ << ", "
-               << alpha_c << ", "
-               << beta_c;
+               << Q_upper_ << ", "
+               << alpha_upper << ", "
+               << beta_upper << ", "
+               << Q_lower_ << ", "
+               << alpha_lower << ", "
+               << beta_lower;
                
 
 }
 
-const Foam::dictionary& Foam::pinballRotatingWallVelocityFvPatchVectorField::getProbesDict()
+const Foam::dictionary& Foam::cylinderJetVelocityFvPatchVectorField::getProbesDict()
 {
     const dictionary& funcDict = this->db().time().controlDict().subDict("functions");
     if (!funcDict.found(probes_name_))
@@ -350,13 +305,13 @@ const Foam::dictionary& Foam::pinballRotatingWallVelocityFvPatchVectorField::get
     return funcDict.subDict(probes_name_);
 }
 
-Foam::probes Foam::pinballRotatingWallVelocityFvPatchVectorField::initializeProbes()
+Foam::probes Foam::cylinderJetVelocityFvPatchVectorField::initializeProbes()
 {
     const dictionary& probesDict = getProbesDict();
     return Foam::probes("probes", this->db().time(), probesDict, false, true);
 }
 
-Foam::timeControl Foam::pinballRotatingWallVelocityFvPatchVectorField::initializeControl()
+Foam::timeControl Foam::cylinderJetVelocityFvPatchVectorField::initializeControl()
 {
     const dictionary& probesDict = getProbesDict();
     start_time_ = probesDict.getOrDefault<scalar>("timeStart", 0.0);
@@ -364,7 +319,7 @@ Foam::timeControl Foam::pinballRotatingWallVelocityFvPatchVectorField::initializ
     return Foam::timeControl(this->db().time(), probesDict, "execute");
 }
 
-void Foam::pinballRotatingWallVelocityFvPatchVectorField::initializeCylinderSegmentation()
+void Foam::cylinderJetVelocityFvPatchVectorField::initializeCylinderSegmentation()
 {
     const fvMesh& mesh(patch().boundaryMesh().mesh());
     // patch name and radius are currently hardcoded
@@ -389,7 +344,7 @@ void Foam::pinballRotatingWallVelocityFvPatchVectorField::initializeCylinderSegm
             faces_a_.append(faceI);
         }
 
-        if (y < o_y_) //upper
+        if (y > o_y_) //upper
         {
             centers_b_.append(Cf.boundaryField()[patchID][faceI]);
             normals_b_.append(Sf.boundaryField()[patchID][faceI]/magSf.boundaryField()[patchID][faceI]);
